94aa1677b9c7fc937c3f819f2837ed669173ad83
Chawangmushi <satoshi0112@gmail.com>
2021-07-19 17:46:39 +0900

    usersとitemsのマイグレーションファイルの編集

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,12 @@
 class Item < ApplicationRecord
+
+ #validates :name, presence: true
+ #validates :description, presence: true
+ #validates :price, presence: true
+
+
+  #belongs_to :user
+
+
+
 end
--- db/migrate/20210719080457_devise_create_users.rb
+++ db/migrate/20210719080457_devise_create_users.rb
@@ -4,8 +4,15 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
+      #t.string :nickname,               null: false
+      #t.string :surname,                null: false
+      #t.string :givenname,              null: false
+      #t.string :surname_furigana,       null: false
+      #t.string :givenname_furigana,     null: false
+      #t.date   :birthday,               null: false
+
+      t.string :email,                  null: false, default: ""
+      t.string :encrypted_password,     null: false, default: ""
 
       ## Recoverable
       t.string   :reset_password_token
