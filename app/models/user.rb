365c70e04066212046c08999668e2a99ef1eb0f4
Chawangmushi <satoshi0112@gmail.com>
2021-07-19 22:02:23 +0900

    名前と名字は全角ひらがな、カタカナ、漢字のみ、フリガナは全角カタカナのみ許可

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname,            presence: true
-  validates :surname,             presence: true
-  validates :givenname,           presence: true
-  validates :surname_furigana,    presence: true 
-  validates :givenname_furigana,  presence: true
+  validates :surname,             presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/}
+  validates :givenname,           presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/}
+  validates :surname_furigana,    presence: true, format: {with: /\A[ァ-ヶー－]+\z/}
+  validates :givenname_furigana,  presence: true, format: {with: /\A[ァ-ヶー－]+\z/}
   validates :birthday,            presence: true
   validates :encrypted_password,:password,:password_confirmation,format:{with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]/}
 
