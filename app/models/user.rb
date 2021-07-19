2b9c29f527e9ff6f03abbd319ecd78d93d705421
Chawangmushi <satoshi0112@gmail.com>
2021-07-19 21:43:10 +0900

    ユーザー登録できるようにした。

---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :nickname,            presence: true
+  validates :surname,             presence: true
+  validates :givenname,           presence: true
+  validates :surname_furigana,    presence: true 
+  validates :givenname_furigana,  presence: true
+  validates :birthday,            presence: true
+
+
+
+end
