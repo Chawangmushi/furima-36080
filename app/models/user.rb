8d18bdc5d21ad84d82fdaf0c26fb64bb677d1772
Chawangmushi <satoshi0112@gmail.com>
2021-07-20 19:26:11 +0900

    バリデーションをwith optionを使ってまとめた。

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,20 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname,            presence: true
-  validates :surname,             presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]\z/ }
-  validates :givenname,           presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]\z/ }
-  validates :surname_furigana,    presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
-  validates :givenname_furigana,  presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
-  validates :birthday,            presence: true
-  validates :encrypted_password, :password, :password_confirmation, format: { with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]/ }
+  with_options presence: true do
+    validates :nickname
+
+    with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } do
+      validates :surname
+      validates :givenname
+    end
+
+    with_options format: { with: /\A[ァ-ヶー－]+\z/ } do
+      validates :surname_furigana
+      validates :givenname_furigana
+    end
+    
+    validates :birthday
+  end
+    validates :encrypted_password, :password, :password_confirmation, format: { with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]/ }
 end
