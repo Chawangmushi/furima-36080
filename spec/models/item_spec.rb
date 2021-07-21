2dd904b5b8adc47e572d83d48205e610ddacb0a5
Chawangmushi <satoshi0112@gmail.com>
2021-07-22 08:40:46 +0900

    itemモデルの単体テストコード内で数字は文字列から数値に変更

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -26,31 +26,31 @@ RSpec.describe Item, type: :model do
       end
 
       it 'カテゴリー(category_id)が未選択（初期値のまま）だと出品できない' do
-        @item.category_id = '1'
+        @item.category_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Category can't be blank")
       end
 
       it '商品の状態(condition_id)が未選択（初期値のまま）だと出品できない' do
-        @item.condition_id = '1'
+        @item.condition_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Condition can't be blank")
       end
 
       it '配送料の負担(shipping_payment_id)が未選択（初期値のまま）だと出品できない' do
-        @item.shipping_payment_id = '1'
+        @item.shipping_payment_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Shipping payment can't be blank")
       end
 
       it '発送元の地域(shipping_from_id)が未選択（初期値のまま）だと出品できない' do
-        @item.shipping_from_id = '1'
+        @item.shipping_from_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Shipping from can't be blank")
       end
 
       it '発送までの日数(shipping_day_id)が未選択（初期値のまま）だと出品できない' do
-        @item.shipping_day_id = '1'
+        @item.shipping_day_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Shipping day can't be blank")
       end
@@ -62,13 +62,13 @@ RSpec.describe Item, type: :model do
       end
 
       it '価格(price)が299以下だと出品できない' do
-        @item.price = '299'
+        @item.price = 299
         @item.valid?
         expect(@item.errors.full_messages).to include('Price is out of setting range')
       end
 
       it '価格(price)が10,000,000以上と出品できない' do
-        @item.price = '10000000'
+        @item.price = 10000000
         @item.valid?
         expect(@item.errors.full_messages).to include('Price is out of setting range')
       end
