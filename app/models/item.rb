38a12356cd6c0a915fa1a6d0d1d32e7fb3a2b300
Chawangmushi <satoshi0112@gmail.com>
2021-07-21 16:44:50 +0900

    配送料の負担、都道府県、配送までの日数を選べるようにした。

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,10 +5,17 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :condition
+  belongs_to :shipping_payment
+  belongs_to :shipping_from
+  belongs_to :shipping_day
 
 
+  #with_options presence: true do
   validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :condition_id, numericality: { other_than: 1 , message: "can't be blank"}
-
+  validates :shipping_payment_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :shipping_from_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :shipping_day_id, numericality: { other_than: 1 , message: "can't be blank"}
+  #end
   
 end
