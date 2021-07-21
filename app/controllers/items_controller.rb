5133d73e589b4dc8d38540c0029f764f762cc19f
Chawangmushi <satoshi0112@gmail.com>
2021-07-21 14:37:04 +0900

    Activahash Gemの追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,19 @@
 class ItemsController < ApplicationController
   def index
   end
+
+
+
+  
+  
+  private
+
+#  def item_params
+#    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
+#  end
+
+#def article_params
+#  params.require(:article).permit(:title,:text,:genre_id)
+#end
+
 end
