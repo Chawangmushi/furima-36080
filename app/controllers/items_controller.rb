8ec63155fd3a17cc5d9295d3317834a917f6864a
Chawangmushi <satoshi0112@gmail.com>
2021-07-21 17:22:57 +0900

    出品機能の仮実装（まだユーザーIDが入っていないのでデータベースに保存はできない）

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,14 +3,29 @@ class ItemsController < ApplicationController
   end
 
 
+  def new
+    @item = Item.new
+
+  end
+  
+  def create
+    @item = Item.new(item_params)
+    binding.pry
+    if @item.save
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
+  end
+
+
 
   
   
   private
-
-#  def item_params
-#    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
-#  end
+    def item_params
+      params.require(:item).permit(:image, :name, :description, :price, :category_id, :condition_id, :shipping_payment_id, :shipping_from_id, :shipping_day_id)#.merge(user_id: current_user.id)
+    end
 
 #def article_params
 #  params.require(:article).permit(:title,:text,:genre_id)
--- app/models/item.rb
+++ app/models/item.rb
@@ -10,12 +10,17 @@ class Item < ApplicationRecord
   belongs_to :shipping_day
 
 
+  validates :image,                         presence: true
+  validates :name,                          presence: true
+  validates :description,                   presence: true
+  validates :price,                         presence: true, inclusion: { in: 300..9_999_999 }, format: { with: /\A[0-9]+\z/ }
+
   #with_options presence: true do
-  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :condition_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :shipping_payment_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :shipping_from_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :shipping_day_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :category_id,                   numericality: { other_than: 1 , message: "can't be blank"}
+  validates :condition_id,                  numericality: { other_than: 1 , message: "can't be blank"}
+  validates :shipping_payment_id,           numericality: { other_than: 1 , message: "can't be blank"}
+  validates :shipping_from_id,              numericality: { other_than: 1 , message: "can't be blank"}
+  validates :shipping_day_id,               numericality: { other_than: 1 , message: "can't be blank"}
   #end
   
 end
