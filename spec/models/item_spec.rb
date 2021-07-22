6a50b9bb10d4d201e6954f426421d783faf90193
Chawangmushi <satoshi0112@gmail.com>
2021-07-22 11:37:11 +0900

    itemモデルの単体テスト、ユーザーが紐付いていないと出品できないテストを追加

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -114,6 +114,13 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include("Image can't be blank")
       end
+
+      it 'ユーザー情報がないと出品できない' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("User must exist")
+      end
+
     end
   end
 end
