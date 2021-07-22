cfb99fe3fc28ba767f8acf30aaf50a993d04a15e
Chawangmushi <satoshi0112@gmail.com>
2021-07-22 12:02:06 +0900

    てすと

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -116,7 +116,7 @@ RSpec.describe Item, type: :model do
       end
 
       it 'ユーザー情報がないと出品できない' do
-        @item.user = nil test
+        @item.user = nil
         @item.valid?
         expect(@item.errors.full_messages).to include("User must exist")
       end
