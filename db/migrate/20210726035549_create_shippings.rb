a1a740efc080ff315e7d044c310966a8f482f85e
Chawangmushi <satoshi0112@gmail.com>
2021-07-26 13:14:26 +0900

    マイグレーション修正２

--- db/migrate/20210726035549_create_shippings.rb
+++ db/migrate/20210726035549_create_shippings.rb
@@ -1,7 +1,5 @@
 class CreateShippings < ActiveRecord::Migration[6.0]
   def change
-    create_table :shippings do |t|
-
       create_table :addresses do |t|
         t.string :postalcode,          null: false
         t.integer :shipping_from_id,   null: false
@@ -12,6 +10,5 @@ class CreateShippings < ActiveRecord::Migration[6.0]
         t.references :order,           null: false, foreign_key: true
 
         t.timestamps
-    end
   end
 end
