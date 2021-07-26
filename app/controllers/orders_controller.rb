4662ce435c56044c466a23fb92a9173ddfc3a233
Chawangmushi <satoshi0112@gmail.com>
2021-07-26 14:22:16 +0900

    ルーティングのネスト

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,6 +2,7 @@ class OrdersController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    @item = Item.find(params[:item_id])
   end
 
   def new
@@ -9,5 +10,9 @@ class OrdersController < ApplicationController
 
   def create
   end
+
+  #def order_params
+    #params.require(:item_order).permit(:postal_code, :prefecture_id, :city_name, :block_name, :building_name, :phone_nember).merge(token: params[:token])
+  #end
 
 end
